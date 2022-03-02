0ab58cc9f6ed7ee0c491cb195f4f60dc809f3f77
Adrián Fernández Alonso <uo264268@uniovi.es>
2022-03-02 16:59:25 +0100

    Metodo post para añadir productos y metodos para listar y añadir users

--- restapi/api.ts
+++ restapi/api.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import {check} from 'express-validator';
 import mongoose from 'mongoose';
+import { userInfo } from 'os';
 import Usuario from './schemas/Usuario';
 
 const Producto = require('./schemas/Producto')
@@ -51,11 +52,47 @@ api.post(
 );*/
 
 api.get(
+  "/users/list",
+  async (req: Request, res: Response): Promise<Response> => {
+      let users = await Usuario.find().select("nombre").select("dni").select("email") 
+      return res.status(200).send(users);
+  }
+);
+
+api.post( 
+  "/users/add",
+  async (req: Request, res: Response): Promise<Response> => {
+    let usuario = new Usuario();
+    usuario.nombre = req.body.nombre;
+    usuario.email = req.body.email;
+    usuario.dni = req.body.dni;
+    usuario.contraseña = req.body.contraseña;
+    
+    await usuario.save();
+    return res.sendStatus(200); 
+  }
+);
+
+api.get(
   "/products/list",
   async (req: Request, res: Response): Promise<Response> => {
-      var productos = await Producto.find().select("nombre").select("origen").select("precio").select("descripcion")
+      let productos = await Producto.find().select("nombre").select("origen").select("precio").select("descripcion")
       return res.status(200).send(productos)
   }
 ); 
+
+api.post(
+  "/products/add",
+  async (req: Request, res: Response): Promise<Response> => {
+    let producto = new Producto();
+    producto.nombre = req.body.nombre;
+    producto.origen = req.body.origen;
+    producto.precio = req.body.precio;
+    producto.descripcion = req.body.descripcion;
+    
+    await producto.save(); 
+    return res.sendStatus(200);
+  }
+);
 
 export default api;
