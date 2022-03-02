9cdc1531e283b606f3b3db0009123b60bc403b40
Adrián Fernández Alonso <uo264268@uniovi.es>
2022-03-02 17:08:40 +0100

    actualizados metodos de webapp/api para las nuevas peticiones

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,11 +1,11 @@
-import {User} from '../shared/shareddtypes';
+import {User, Product} from '../shared/shareddtypes';
 
 export async function addUser(user:User):Promise<boolean>{
     const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
     let response = await fetch(apiEndPoint+'/users/add', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({'name':user.name, 'email':user.email})
+        body: JSON.stringify({'name':user.name, 'email':user.email, 'contraseña':user.password, 'dni':user.dni})
       });
     if (response.status===200)
       return true;
@@ -18,4 +18,24 @@ export async function getUsers():Promise<User[]>{
     let response = await fetch(apiEndPoint+'/users/list');
     //The objects returned by the api are directly convertible to User objects
     return response.json()
+}
+
+export async function addProduct(product:Product):Promise<boolean>{
+  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+  let response = await fetch(apiEndPoint+'/users/add', {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({'name':product.name, 'origin':product.origin, 'descripcion':product.description, 'precio':product.price})
+    });
+  if (response.status===200)
+    return true;
+  else
+    return false;
+}
+
+export async function getProducts():Promise<User[]>{
+  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+  let response = await fetch(apiEndPoint+'/users/list');
+  //The objects returned by the api are directly convertible to User objects
+  return response.json()
 }
--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,4 +1,13 @@
 export type User = {
     name:string;
     email:string;
-  }
+    password:string;
+    dni:string;
+}
+
+export type Product = {
+    name:string;
+    description:string;
+    origin:string;
+    price:number;
+}
