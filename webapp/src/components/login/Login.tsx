2d15c590af987f1a1cbd814cfd0e8bb84a5d8cfd
Diego Martínez <diegomarty1200@gmail.com>
2022-03-20 17:07:14 +0100

    Parte de Solid

--- webapp/src/components/login/Login.tsx
+++ webapp/src/components/login/Login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -15,6 +15,8 @@ import  {login} from '../../api/api';
 import { Navigate } from "react-router-dom";
 import {LoginData} from '../../shared/shareddtypes';
 import { Api } from '@mui/icons-material';
+import { LoginSolid } from "@inrupt/solid-ui-react";
+
 
 function Copyright(props: any) {
   return (
@@ -29,10 +31,15 @@ function Copyright(props: any) {
 const theme = createTheme();
 
 const Login:React.FC=()=> {
-  const [email, setEmail] = useState("");
-  const [contraseña, setContraseña] = useState("");
+  const [email, setEmail] = useState("https://inrupt.net");
+  const [contraseña, setContraseña] = useState("https://inrupt.net");
   const [logueado, setLogueado] = useState("");
   const [errorMessage, setErrorMessage] = useState('');
+  const [url, setUrl] = useState("https://localhost:3000");
+
+  useEffect(() => {
+    setUrl(window.location.href);
+  }, [setUrl]);
   
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -96,6 +103,10 @@ const Login:React.FC=()=> {
             {errorMessage && (
               <p className="error"> {errorMessage} </p>
             )}
+            <LoginSolid
+              oidcIssuer={email}
+              redirectUrl={url}
+            >
             <Button
               type="submit"
               fullWidth
