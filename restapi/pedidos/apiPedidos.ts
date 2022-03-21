ecf80d7f8ef9f90b9d49777a31d3ef89a8bd4209
Adrián Fernández Alonso <adrianfutbolin@gmail.com>
2022-03-21 11:26:04 +0100

    Peticion añadir pedido

--- restapi/pedidos/apiPedidos.ts
+++ restapi/pedidos/apiPedidos.ts
@@ -3,4 +3,33 @@ import Pedido from './PedidoSchema';
 
 const api:Router = express.Router();
 
+api.post(
+    "/pedidos/add",
+    async (req: Request, res: Response): Promise<Response> => {
+      try {
+        let pedido = new Pedido();
+        
+        pedido.numero_pedido = req.body.numero_pedido;
+        pedido.id_usuario = req.body.id_usuario;
+
+        pedido.lista_productos = req.body.lista_productos;
+        pedido.precio_total = req.body.precio_total;
+        
+        pedido.direccion = req.body.direccion;
+        // pedido.direccion.calle = req.body.calle;
+        // pedido.direccion.localidad = req.body.localidad;
+        // pedido.direccion.provincia = req.body.provincia;
+        // pedido.direccion.pais = req.body.pais;
+        // pedido.direccion.codigo_postal = req.body.codigo_postal;
+
+        pedido.fecha = new Date();
+
+        await pedido.save();
+        return res.status(200).send(pedido);
+      } catch {
+        return res.sendStatus(500);
+      }
+    }
+  );
+
 export default api;
