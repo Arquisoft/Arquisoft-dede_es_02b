a1fb3adae35e159a4171e5de34fc81926746af7e
UO277188 <uo277188@uniovi.es>
2022-03-29 12:09:41 +0200

    Resumen pedido

--- webapp/src/components/carrito/Carrito.tsx
+++ webapp/src/components/carrito/Carrito.tsx
@@ -3,7 +3,9 @@ import {Product} from '../../shared/shareddtypes';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import ProductComponent from './CarritoItem';
-
+import Split from 'react-split';
+import { Typography } from '@mui/material';
+import Total from './Total';
 
 type ProductProps = {
   products: Product[];
@@ -13,13 +15,14 @@ let carrito = new Map<Product, number>();
 
 let cantidad:number = 0;
 
- export function getPrecio():number{
+export function getPrecio():number{
   let precio = 0;
   carrito.forEach((value: number, key: Product) => {
     precio += key.precio * value;
   });
   return precio;
 }
+
 const Carrito: React.FC<ProductProps>= (props: ProductProps) =>{
   var i = 0;
   let productos: Product[]=[];
@@ -35,16 +38,50 @@ const Carrito: React.FC<ProductProps>= (props: ProductProps) =>{
     }
   }
 
+  function ListaResumen(props: any){
+    const items = props.items;
+    let lista: any;
+    items.forEach((value: number, key: Product) => {
+      lista += <li>key.nombre</li>
+    });
+    return (
+      <ul>{lista}</ul>
+    );
+  }
+
   return (
-    <Box sx={{ flexGrow: 1, padding: 3}}>
-      <Grid container spacing={3}>
-        {Array.from(Array(productos.length)).map((_, index) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-            <ProductComponent product={productos[index]} cantidadItem={carrito.get(productos[index])as number}/>
+    <Split
+      sizes={[75, 25]}
+      direction="horizontal"
+      cursor="col-resize"
+      className="split-flex"
+      minSize={[1000, 500]}
+    >
+      <Box sx={{ flexGrow: 1, padding: 3}}>
+          <Grid container spacing={3}>
+            {Array.from(Array(productos.length)).map((_, index) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                <ProductComponent product={productos[index]} cantidadItem={carrito.get(productos[index])as number}/>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Box>
+      </Box>
+      <Box sx={{ flexGrow: 1, padding: 3}}>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Resumen del pedido:
+          </Typography>
+          {Array.from(Array(productos.length)).map((_, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <li>{productos[index].nombre +" ("+ carrito.get(productos[index]) + " uds)" +
+                    "  -------  "+productos[index].precio*(carrito.get(productos[index])as number)+"€"}</li>
+            </Grid>
+          ))}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            ----------------------------
+          </Typography>
+          <Total></Total>
+      </Box>
+    </Split>
   ); 
 }
 
