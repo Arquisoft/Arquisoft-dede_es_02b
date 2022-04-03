6f053cc29e78c74b0bc2680756132f26f267de12
Pedro Zahonero Mangas <UO276840@uniovi.es>
2022-04-03 11:08:38 +0200

    Test Productos

--- webapp/src/tests/product.test.tsx
+++ webapp/src/tests/product.test.tsx
@@ -3,10 +3,11 @@ import { render, screen } from '@testing-library/react';
 import Products from '../components/product/Products';
 import { getProducts } from '../api/api';
 import { Product } from '../shared/shareddtypes';
-import products from './mockData.json'
+import products from './mockData.json';
+import { BrowserRouter } from 'react-router-dom';
 
 test('Productos', () => {
-  render(<Products products ={products.productos}/>);
+  render(<BrowserRouter><Products products ={products.productos}/></BrowserRouter>);
   products.productos.forEach(element => {
     const linkElement = screen.getByText(element.nombre);
     expect(linkElement).toBeInTheDocument();
