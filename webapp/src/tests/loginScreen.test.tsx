eab1fe1eb52ab21c1ba2486dce0b33e68c9d1f07
Pedro Zahonero Mangas <UO276840@uniovi.es>
2022-04-07 17:24:34 +0200

    Arreglo de test

--- webapp/src/tests/loginScreen.test.tsx
+++ webapp/src/tests/loginScreen.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LoginScreen from '../components/login/LoginScreen';
 
+import { MemoryRouter } from 'react-router-dom';
+
 test('Login screen', () => {
-    render(<LoginScreen/>);
+    render(<MemoryRouter><LoginScreen/></MemoryRouter>);
     let text = screen.getByText(/DeDe/);
     expect(text).toBeInTheDocument();
     text = screen.getByText(/Inicia sesión/);
