ebb144023e609404f28eeaef6635182619cb5be3
Pedro Zahonero Mangas <UO276840@uniovi.es>
2022-05-01 20:19:38 +0200

    Vista resumen total del pedido

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -12,6 +12,7 @@ import HomeNavBar from './components/HomeNavBar';
 import Error404 from './components/error/Error404';
 import EditarUsuario from './components/usuario/EditarUsuario';
 import Pago from './components/pago/Pago';
+import ResumenPedido from './components/pedidos/ResumenPedido';
 
 function App(): JSX.Element {
 
@@ -67,6 +68,12 @@ function App(): JSX.Element {
           <div>
             <NavBar/>
             <EditarUsuario/>
+          </div>
+        }/> 
+        <Route path="/resumenPedido" element={
+          <div>
+            <NavBar/>
+            <ResumenPedido/>
           </div>
         }/> 
         <Route path="*" element={
