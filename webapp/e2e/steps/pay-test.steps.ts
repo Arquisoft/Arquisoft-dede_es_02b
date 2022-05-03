949079dd694e7140c5dac83c3037ed8a58b1f7f6
Diego Martínez <diegomarty1200@gmail.com>
2022-05-03 22:03:23 +0200

    Actualización test

--- webapp/e2e/steps/pay-test.steps.ts
+++ webapp/e2e/steps/pay-test.steps.ts
@@ -15,63 +15,28 @@ defineFeature(feature, test => {
     page = await browser.newPage();
 
     await page
-        .goto("http://localhost:3000/pago", {
+        .goto("http://localhost:3000/pago/metodoPago", {
           waitUntil: "networkidle0",
         })
         .catch((error) => {console.log(error)});
   });
 
   test('El usuario quiere comprar: Empty form', ({given,when,then}) => {
-    let calle:string;
-    let localidad:string;
-    let provincia:string;
-    let pais:string;
-    let codigo_postal:string;
     let numTarjeta:string;
     let fechaTarjeta:string;
     let numSeguridadTarjeta:string;
 
     given('Un usuario no introduce datos', async() => {
-      calle = "";
-      localidad = "";
-      provincia = "";
-      pais = "";
-      codigo_postal = "";
       numTarjeta = "";
       fechaTarjeta = "";
       numSeguridadTarjeta = "";
     });
 
-    when('I add some products in the cart', async () => {
-      let calleSelector ='[id="calle"]';
-      let localidadSelector = '[id="localidad"]';
-      let provinciaSelector = '[id="provincia"]';
-      let paisSelector = '[id="pais"]';
-      let codigo_postalSelector = '[id="codigo_postal"]';
+    when('No rellanamos el formulario de compra', async () => {
       let numTarjetaSelector = '[id="numTarjeta"]';
       let fechaTarjetaSelector = '[id="fechaTarjeta"]';
       let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
       let botonSelector = '[id="pagar"]';
-
-      await page.waitForSelector(calleSelector);
-      await page.click(calleSelector);
-      await page.keyboard.type(calle);
-
-      await page.waitForSelector(localidadSelector);
-      await page.click(localidadSelector);
-      await  page.keyboard.type(localidad);
-
-      await page.waitForSelector(provinciaSelector);
-      await page.click(provinciaSelector);
-      await page.keyboard.type(provincia);
-
-      await page.waitForSelector(paisSelector);
-      await page.click(paisSelector);
-      await page.keyboard.type(pais);
-
-      await page.waitForSelector(codigo_postalSelector);
-      await page.click(codigo_postalSelector);
-      await page.keyboard.type(codigo_postal);
 
       await page.waitForSelector(numTarjetaSelector);
       await page.click(numTarjetaSelector);
@@ -91,37 +56,22 @@ defineFeature(feature, test => {
 
 
     then('Nos redirige correctamente a la ventana de productos', async () => {
-      await expect(page).toMatch('Pago')
+      await expect(page).toMatch('metodoPago')
     });
   })
 
   test('El usuario quiere comprar: Incorrect Date', ({given,when,then}) => {
-    let calle:string;
-    let localidad:string;
-    let provincia:string;
-    let pais:string;
-    let codigo_postal:string;
     let numTarjeta:string;
     let fechaTarjeta:string;
     let numSeguridadTarjeta:string;
 
     given('Un usuario tiene la tarjea de credito caducada', async() => {
-      calle = "taujo";
-      localidad = "Cancienes";
-      provincia = "Asturias";
-      pais = "spain";
-      codigo_postal = "33470";
       numTarjeta = "1234 1234 1234 1234";
       fechaTarjeta = "2/21";
       numSeguridadTarjeta = "666";
     });
 
     when('No rellanamos el formulario de compra', async () => {
-      let calleSelector ='[id="calle"]';
-      let localidadSelector = '[id="localidad"]';
-      let provinciaSelector = '[id="provincia"]';
-      let paisSelector = '[id="pais"]';
-      let codigo_postalSelector = '[id="codigo_postal"]';
       let numTarjetaSelector = '[id="numTarjeta"]';
       let fechaTarjetaSelector = '[id="fechaTarjeta"]';
       let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
@@ -165,7 +115,7 @@ defineFeature(feature, test => {
 
 
     then('No nos deja realizar la compra y se nos indica los campos obligatorios a rellenar', async () => {
-      await expect(page).toMatch('Pago')
+      await expect(page).toMatch('metodoPago')
     });
   })
 
@@ -239,7 +189,7 @@ defineFeature(feature, test => {
 
 
     then('Nos deja realizar la compra y se nos realiza el pedido', async () => {
-      await expect(page).toMatch('Pago')
+      await expect(page).toMatch('metodoPago')
     });
   })
 
