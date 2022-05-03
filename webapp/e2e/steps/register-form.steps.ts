5a1afddae8a15a7d906e1e9047eec4ba9d7c6c5c
Pedro Zahonero Mangas <UO276840@uniovi.es>
2022-05-03 21:22:47 +0200

    Prueba errores

--- webapp/e2e/steps/register-form.steps.ts
+++ webapp/e2e/steps/register-form.steps.ts
@@ -52,40 +52,49 @@ defineFeature(feature, test => {
       })
 
       await expect(page).toClick('button', { text: 'Completar registro' })
-      // let nombreSelector ='[id="nombre"]';
-      // let apellidosSelector = '[id="apellidos"]';
-      // let dniSelector = '[id="dni"]';
-      // let emailSelector = '[id="email"]';
-      // let contraseñaSelector = '[id="contraseña"]';
-      // let botonSelector = '[id="registrarse"]';
+      let nombreSelector ='[id="nombre"]';
+      let apellidosSelector = '[id="apellidos"]';
+      let dniSelector = '[id="dni"]';
+      let emailSelector = '[id="email"]';
+      let contraseñaSelector = '[id="contraseña"]';
+      let botonSelector = '[id="registrarse"]';
 
-      // await page.waitForSelector(nombreSelector);
-      // await page.click(nombreSelector);
-      // await page.keyboard.type(nombre);
+      await page.waitForSelector(nombreSelector);
+      await page.click(nombreSelector);
+      await page.keyboard.type(nombre);
 
-      // await page.waitForSelector(apellidosSelector);
-      // await page.click(apellidosSelector);
-      // await page.keyboard.type(apellidos);
+      await page.waitForSelector(apellidosSelector);
+      await page.click(apellidosSelector);
+      await page.keyboard.type(apellidos);
 
-      // await page.waitForSelector(dniSelector);
-      // await page.click(dniSelector);
-      // await page.keyboard.type(dni);
+      await page.waitForSelector(dniSelector);
+      await page.click(dniSelector);
+      await page.keyboard.type(dni);
 
-      // await page.waitForSelector(emailSelector);
-      // await page.click(emailSelector);
-      // await page.keyboard.type(email);
+      await page.waitForSelector(emailSelector);
+      await page.click(emailSelector);
+      await page.keyboard.type(email);
 
-      // await page.waitForSelector(contraseñaSelector);
-      // await page.click(contraseñaSelector);
-      // await page.keyboard.type(contraseña);
+      await page.waitForSelector(contraseñaSelector);
+      await page.click(contraseñaSelector);
+      await page.keyboard.type(contraseña);
 
-      // await page.waitForSelector(botonSelector);
-      // await page.click(botonSelector);
+      await page.waitForSelector(botonSelector);
+      await page.click(botonSelector);
     });
 
     then('Nos redirige correctamente a la ventana de productos', async () => {
-      await page.goto("http://localhost:3000/products");
+      //await page.goto("http://localhost:3000/products");
+      await expect(page).toMatch('Error: El nombre no puede estar vacío')
+      await expect(page).toMatch('Error: El dni no puede estar vacío')
+      await expect(page).toMatch('Error: El dni debe contener 9 carácteres')
+      await expect(page).toMatch('Error: El dni introducido ya existe')
+      await expect(page).toMatch('Error: El email no puede estar vacío')
+      await expect(page).toMatch('Error: El email introducido ya existe')
+      await expect(page).toMatch('Error: El formato del email no es válido')
+      await expect(page).toMatch('Error: La contraseña no puede estar vacía')
       await expect(page).toMatch('Productos')
+
     });
   })
 
