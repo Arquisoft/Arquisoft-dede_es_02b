3da51b2723034f1c25123d34bc4f6c7500aab7c6
Diego Martínez <diegomarty1200@gmail.com>
2022-05-03 22:14:28 +0200

    Test Pago (quitar errores)

--- webapp/e2e/steps/pay-test.steps.ts
+++ webapp/e2e/steps/pay-test.steps.ts
@@ -66,8 +66,8 @@ defineFeature(feature, test => {
     let numSeguridadTarjeta:string;
 
     given('Un usuario tiene la tarjea de credito caducada', async() => {
-      numTarjeta = "1234 1234 1234 1234";
-      fechaTarjeta = "2/21";
+      numTarjeta = "1234 1234 1234 123";
+      fechaTarjeta = "02/21";
       numSeguridadTarjeta = "666";
     });
 
@@ -76,26 +76,6 @@ defineFeature(feature, test => {
       let fechaTarjetaSelector = '[id="fechaTarjeta"]';
       let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
       let botonSelector = '[id="pagar"]';
-
-      await page.waitForSelector(calleSelector);
-      await page.click(calleSelector);
-      await page.keyboard.type(calle);
-
-      await page.waitForSelector(localidadSelector);
-      await page.click(localidadSelector);
-      await  page.keyboard.type(localidad);
-
-      await page.waitForSelector(provinciaSelector);
-      await page.click(provinciaSelector);
-      await page.keyboard.type(provincia);
-
-      await page.waitForSelector(paisSelector);
-      await page.click(paisSelector);
-      await page.keyboard.type(pais);
-
-      await page.waitForSelector(codigo_postalSelector);
-      await page.click(codigo_postalSelector);
-      await page.keyboard.type(codigo_postal);
 
       await page.waitForSelector(numTarjetaSelector);
       await page.click(numTarjetaSelector);
@@ -120,56 +100,21 @@ defineFeature(feature, test => {
   })
 
   test('El usuario quiere comprar : All form correct', ({given,when,then}) => {
-    let calle:string;
-    let localidad:string;
-    let provincia:string;
-    let pais:string;
-    let codigo_postal:string;
     let numTarjeta:string;
     let fechaTarjeta:string;
     let numSeguridadTarjeta:string;
 
     given('Un usuario indica sus datos correctamente', async() => {
-      calle = "taujo";
-      localidad = "Cancienes";
-      provincia = "Asturias";
-      pais = "spain";
-      codigo_postal = "33470";
       numTarjeta = "1234 1234 1234 1234";
-      fechaTarjeta = "6/23";
+      fechaTarjeta = "06/23";
       numSeguridadTarjeta = "666";
     });
 
     when('Rellanamos el formulario de compra correctamente', async () => {
-      let calleSelector ='[id="calle"]';
-      let localidadSelector = '[id="localidad"]';
-      let provinciaSelector = '[id="provincia"]';
-      let paisSelector = '[id="pais"]';
-      let codigo_postalSelector = '[id="codigo_postal"]';
       let numTarjetaSelector = '[id="numTarjeta"]';
       let fechaTarjetaSelector = '[id="fechaTarjeta"]';
       let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
       let botonSelector = '[id="pagar"]';
-
-      await page.waitForSelector(calleSelector);
-      await page.click(calleSelector);
-      await page.keyboard.type(calle);
-
-      await page.waitForSelector(localidadSelector);
-      await page.click(localidadSelector);
-      await  page.keyboard.type(localidad);
-
-      await page.waitForSelector(provinciaSelector);
-      await page.click(provinciaSelector);
-      await page.keyboard.type(provincia);
-
-      await page.waitForSelector(paisSelector);
-      await page.click(paisSelector);
-      await page.keyboard.type(pais);
-
-      await page.waitForSelector(codigo_postalSelector);
-      await page.click(codigo_postalSelector);
-      await page.keyboard.type(codigo_postal);
 
       await page.waitForSelector(numTarjetaSelector);
       await page.click(numTarjetaSelector);
