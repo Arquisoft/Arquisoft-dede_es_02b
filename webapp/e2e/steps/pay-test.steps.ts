53bc66c198261d594aa3e81a585bf0e93de18e7f
Diego Martínez <diegomarty1200@gmail.com>
2022-05-03 19:41:23 +0200

    Test e2e - Pago

--- webapp/e2e/steps/pay-test.steps.ts
+++ webapp/e2e/steps/pay-test.steps.ts
@@ -21,93 +21,225 @@ defineFeature(feature, test => {
         .catch((error) => {console.log(error)});
   });
 
-  test('The shopping cart is empty', ({given,when,then}) => {
-    let email:string;
-    let estadoPedido:string;
+  test('El usuario quiere comprar: Empty form', ({given,when,then}) => {
     let calle:string;
     let localidad:string;
     let provincia:string;
     let pais:string;
-    let cp:string;
+    let codigo_postal:string;
     let numTarjeta:string;
-    let cadTarjeta:string;
-    let secTarjeta:string;
+    let fechaTarjeta:string;
+    let numSeguridadTarjeta:string;
 
-    given('An empty cart', async() => {
-      email = "uo270457@uniovi.es"
-      nombre = "Diego"
-      apellidos = "Martinez"
-      dni = "7196503K"
-      contraseña = "Contraseña"
-
-      let nombreSelector ='[id="nombre"]';
-      let apellidosSelector = '[id="apellidos"]';
-      let dniSelector = '[id="dni"]';
-      let emailSelector = '[id="email"]';
-      let contraseñaSelector = '[id="contraseña"]';
-      let botonSelector = '[id="registrarse"]';
-
-      await page.waitForSelector(nombreSelector);
-      await page.click(nombreSelector);
-      await page.keyboard.type(nombre);
-
-      await page.waitForSelector(apellidosSelector);
-      await page.click(apellidosSelector);
-      await  page.keyboard.type(apellidos);
-
-      await page.waitForSelector(dniSelector);
-      await page.click(dniSelector);
-      await page.keyboard.type(dni);
-
-      await page.waitForSelector(emailSelector);
-      await page.click(emailSelector);
-      await page.keyboard.type(email);
-
-      await page.waitForSelector(contraseñaSelector);
-      await page.click(contraseñaSelector);
-      await page.keyboard.type(contraseña);
-
-      await page.waitForSelector(botonSelector);
-      await page.click(botonSelector);
-      await expect(page).toMatch('Productos')
+    given('Un usuario no introduce datos', async() => {
+      calle = "";
+      localidad = "";
+      provincia = "";
+      pais = "";
+      codigo_postal = "";
+      numTarjeta = "";
+      fechaTarjeta = "";
+      numSeguridadTarjeta = "";
     });
 
     when('I add some products in the cart', async () => {
-      let nombreSelector ='[id="addToCart_Mango"]';
-      await expect(page).toMatch('Productos')
-      await page.click(nombreSelector);
+      let calleSelector ='[id="calle"]';
+      let localidadSelector = '[id="localidad"]';
+      let provinciaSelector = '[id="provincia"]';
+      let paisSelector = '[id="pais"]';
+      let codigo_postalSelector = '[id="codigo_postal"]';
+      let numTarjetaSelector = '[id="numTarjeta"]';
+      let fechaTarjetaSelector = '[id="fechaTarjeta"]';
+      let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
+      let botonSelector = '[id="pagar"]';
 
-      nombreSelector ='[id="addToCart_Pera"]';
-      await page.click(nombreSelector);
+      await page.waitForSelector(calleSelector);
+      await page.click(calleSelector);
+      await page.keyboard.type(calle);
 
-      nombreSelector ='[id="addUnit_Sandía"]';
-      await page.click(nombreSelector);
+      await page.waitForSelector(localidadSelector);
+      await page.click(localidadSelector);
+      await  page.keyboard.type(localidad);
 
-      nombreSelector ='[id="addUnit_Sandía"]';
-      await page.click(nombreSelector);
+      await page.waitForSelector(provinciaSelector);
+      await page.click(provinciaSelector);
+      await page.keyboard.type(provincia);
 
-      nombreSelector ='[id="addToCart_Sandía"]';
-      await page.click(nombreSelector);
+      await page.waitForSelector(paisSelector);
+      await page.click(paisSelector);
+      await page.keyboard.type(pais);
+
+      await page.waitForSelector(codigo_postalSelector);
+      await page.click(codigo_postalSelector);
+      await page.keyboard.type(codigo_postal);
+
+      await page.waitForSelector(numTarjetaSelector);
+      await page.click(numTarjetaSelector);
+      await page.keyboard.type(numTarjeta);
+
+      await page.waitForSelector(fechaTarjetaSelector);
+      await page.click(fechaTarjetaSelector);
+      await page.keyboard.type(fechaTarjeta);
+
+      await page.waitForSelector(numSeguridadTarjetaSelector);
+      await page.click(numSeguridadTarjetaSelector);
+      await page.keyboard.type(numSeguridadTarjeta);
+
+      await page.waitForSelector(botonSelector);
+      await page.click(botonSelector);
     });
 
-    then('The products should appear in the cart window', async () => {
-      // await expect(page).toMatch('You have been registered in the system!')
-      let nombreSelector ='[id="goToCart"]';
-      await page.click(nombreSelector);
-      await expect(page).toMatch('Carrito')
-      await expect(page).toMatch('Pera');
-      await expect(page).toMatch('Sandía');
 
-      await expect(page).toMatch('Total: €7.32')
+    then('Nos redirige correctamente a la ventana de productos', async () => {
+      await expect(page).toMatch('Pagos')
+    });
+  })
 
-      nombreSelector ='[id="removeAll_Pera"]';
-      await page.click(nombreSelector);
-      await expect(page).toMatch('Total: €5.97');
+  test('El usuario quiere comprar: Incorrect Date', ({given,when,then}) => {
+    let calle:string;
+    let localidad:string;
+    let provincia:string;
+    let pais:string;
+    let codigo_postal:string;
+    let numTarjeta:string;
+    let fechaTarjeta:string;
+    let numSeguridadTarjeta:string;
 
-      nombreSelector ='[id="clearCart"]';
-      await page.click(nombreSelector);
+    given('Un usuario tiene la tarjea de credito caducada', async() => {
+      calle = "taujo";
+      localidad = "Cancienes";
+      provincia = "Asturias";
+      pais = "spain";
+      codigo_postal = "33470";
+      numTarjeta = "1234 1234 1234 1234";
+      fechaTarjeta = "2/21";
+      numSeguridadTarjeta = "666";
+    });
 
-      await expect(page).toMatch('Total: €0.00');
+    when('No rellanamos el formulario de compra', async () => {
+      let calleSelector ='[id="calle"]';
+      let localidadSelector = '[id="localidad"]';
+      let provinciaSelector = '[id="provincia"]';
+      let paisSelector = '[id="pais"]';
+      let codigo_postalSelector = '[id="codigo_postal"]';
+      let numTarjetaSelector = '[id="numTarjeta"]';
+      let fechaTarjetaSelector = '[id="fechaTarjeta"]';
+      let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
+      let botonSelector = '[id="pagar"]';
+
+      await page.waitForSelector(calleSelector);
+      await page.click(calleSelector);
+      await page.keyboard.type(calle);
+
+      await page.waitForSelector(localidadSelector);
+      await page.click(localidadSelector);
+      await  page.keyboard.type(localidad);
+
+      await page.waitForSelector(provinciaSelector);
+      await page.click(provinciaSelector);
+      await page.keyboard.type(provincia);
+
+      await page.waitForSelector(paisSelector);
+      await page.click(paisSelector);
+      await page.keyboard.type(pais);
+
+      await page.waitForSelector(codigo_postalSelector);
+      await page.click(codigo_postalSelector);
+      await page.keyboard.type(codigo_postal);
+
+      await page.waitForSelector(numTarjetaSelector);
+      await page.click(numTarjetaSelector);
+      await page.keyboard.type(numTarjeta);
+
+      await page.waitForSelector(fechaTarjetaSelector);
+      await page.click(fechaTarjetaSelector);
+      await page.keyboard.type(fechaTarjeta);
+
+      await page.waitForSelector(numSeguridadTarjetaSelector);
+      await page.click(numSeguridadTarjetaSelector);
+      await page.keyboard.type(numSeguridadTarjeta);
+
+      await page.waitForSelector(botonSelector);
+      await page.click(botonSelector);
+    });
+
+
+    then('No nos deja realizar la compra y se nos indica los campos obligatorios a rellenar', async () => {
+      await expect(page).toMatch('Pagos')
+    });
+  })
+
+  test('El usuario quiere comprar : All form correct', ({given,when,then}) => {
+    let calle:string;
+    let localidad:string;
+    let provincia:string;
+    let pais:string;
+    let codigo_postal:string;
+    let numTarjeta:string;
+    let fechaTarjeta:string;
+    let numSeguridadTarjeta:string;
+
+    given('Un usuario indica sus datos correctamente', async() => {
+      calle = "taujo";
+      localidad = "Cancienes";
+      provincia = "Asturias";
+      pais = "spain";
+      codigo_postal = "33470";
+      numTarjeta = "1234 1234 1234 1234";
+      fechaTarjeta = "6/23";
+      numSeguridadTarjeta = "666";
+    });
+
+    when('Rellanamos el formulario de compra correctamente', async () => {
+      let calleSelector ='[id="calle"]';
+      let localidadSelector = '[id="localidad"]';
+      let provinciaSelector = '[id="provincia"]';
+      let paisSelector = '[id="pais"]';
+      let codigo_postalSelector = '[id="codigo_postal"]';
+      let numTarjetaSelector = '[id="numTarjeta"]';
+      let fechaTarjetaSelector = '[id="fechaTarjeta"]';
+      let numSeguridadTarjetaSelector = '[id="numSeguridadTarjeta"]';
+      let botonSelector = '[id="pagar"]';
+
+      await page.waitForSelector(calleSelector);
+      await page.click(calleSelector);
+      await page.keyboard.type(calle);
+
+      await page.waitForSelector(localidadSelector);
+      await page.click(localidadSelector);
+      await  page.keyboard.type(localidad);
+
+      await page.waitForSelector(provinciaSelector);
+      await page.click(provinciaSelector);
+      await page.keyboard.type(provincia);
+
+      await page.waitForSelector(paisSelector);
+      await page.click(paisSelector);
+      await page.keyboard.type(pais);
+
+      await page.waitForSelector(codigo_postalSelector);
+      await page.click(codigo_postalSelector);
+      await page.keyboard.type(codigo_postal);
+
+      await page.waitForSelector(numTarjetaSelector);
+      await page.click(numTarjetaSelector);
+      await page.keyboard.type(numTarjeta);
+
+      await page.waitForSelector(fechaTarjetaSelector);
+      await page.click(fechaTarjetaSelector);
+      await page.keyboard.type(fechaTarjeta);
+
+      await page.waitForSelector(numSeguridadTarjetaSelector);
+      await page.click(numSeguridadTarjetaSelector);
+      await page.keyboard.type(numSeguridadTarjeta);
+
+      await page.waitForSelector(botonSelector);
+      await page.click(botonSelector);
+    });
+
+
+    then('Nos deja realizar la compra y se nos realiza el pedido', async () => {
+      await expect(page).toMatch('Pagos')
     });
   })
 
